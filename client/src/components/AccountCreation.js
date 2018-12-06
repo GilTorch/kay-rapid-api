8512b7216c33c358ba0f13f34b22bc7777b5a38c
Gilbert Torchon <gilbert.torchon@esih.edu>
2018-12-06 11:14:53 -0600

    User can log out. User is notified (inside the app) when account is created

--- client/src/components/AccountCreation.js
+++ client/src/components/AccountCreation.js
@@ -4,6 +4,14 @@ import { Link } from 'react-router-dom';
 import {Mutation} from 'react-apollo';
 import { ACCOUNT_CREATION } from '../queries/queries';
 
+const warningStyle={
+    backgroundColor:"green",
+    color:"white",
+    display:"flex",
+    justifyContent:"center",
+    alignItems:"center"
+}
+
 const AccountCreation = ()=>{
 
         let emailInput;
@@ -18,6 +26,15 @@ const AccountCreation = ()=>{
                 (signup, {data}) =>(
 
                 <div className="stack-screen account-creation-screen">
+                    {
+                        data!==undefined?
+                        <div style={{textAlign:"center"}}>KONT OU AN KREYE</div>
+                        :""
+                    }
+                    {/* // <div className="add-house-card__warning-message" style={warningStyle}>
+                    //     <span>&times;</span>
+                    //     <p>KONT OU KREYE KOUNYA</p>
+                    // </div> */}
                     <Link to="/authentication/sign-in-with-social-media"> 
                         <button className="close-icon">&times;</button>
                     </Link>
@@ -40,7 +57,7 @@ const AccountCreation = ()=>{
                         <div className="account-creation-screen__form-group"><label className="account-creation-screen__label">Imèl</label><input placeholder="Tanpri mete imèl ou " type="email" className="account-creation-screen__input" ref={node => {emailInput = node;}}/></div>
                         <div className="account-creation-screen__form-group"><label className="account-creation-screen__label">Paswòd</label><input placeholder="Chwazi yon paswòd  " type="password" className="account-creation-screen__input" ref={node => {passwordInput = node;}}/></div>
                         <div className="account-creation-screen__form-group"><label className="account-creation-screen__label">Konfime Paswòd</label><input placeholder="Konfime paswòd ou fenk mete a " type="password" className="account-creation-screen__input" /></div>
-                        <div className="account-creation-screen__form-group"><button className="authentication__button signin-without-socialmedia-screen__button" type="submit">KREYE KONT LAN</button></div>
+                        <div className="account-creation-screen__form-group"><button className="auth-button success-button" type="submit">KREYE KONT LAN</button></div>
                     </form>
                 </div>
 
