0f4839383cb0d9b1955a06ed5d843875c6b4b465
Gilbert Torchon <gilbert.torchon@esih.edu>
2018-12-23 13:53:03 -0600

    fixed cache writing after loading

--- client/src/components/SignInWithoutSocialMedia.js
+++ client/src/components/SignInWithoutSocialMedia.js
@@ -5,10 +5,12 @@ import { AUTH_WITHOUT_SOCIAL_MEDIA,WRITE_AUTH_INFO } from '../queries/queries';
 
 import Loading from './Loading';
 
+import notify from '../utils/notify';
+import { ToastContainer } from 'react-toastify';
+
 
 const SignInWithoutSocialMedia = ({ signIn,writeUserAuthInfoToCache,history })=>{
-
- 
+    
     return(
         <Mutation mutation={ AUTH_WITHOUT_SOCIAL_MEDIA }>
         {(login,{loading,error})=>(
@@ -33,18 +35,17 @@ const SignInWithoutSocialMedia = ({ signIn,writeUserAuthInfoToCache,history })=>
                                 email:login.user.email,
                                 profilePicture:login.user.profilePicture.url
                             };
-                        
-                            writeUserAuthInfoToCache({variables:{ userAuthInfo: userObject }})
-                            
+                            writeUserAuthInfoToCache({variables:{ userAuthInfo: userObject }});                           
                         }
-                    }).then(()=>{history.push("/profile")})
+                    }).then(()=>{history.push('/profile')})
                     
                 }} className="signin-without-socialmedia-screen__form">
                     {(loading)?<Loading/>:""}
-                    {error && <p>Gen on erreu :( SVP reseye</p>}
+                    {(error)? notify("GEN ON EREU KI PASE. VERIFYE KONEKSYON ENTENET OU EPI REKOMANSE ON LOT MOMAN","error"):""}
                     <div className="signin-without-socialmedia-screen__form-group"><label className="signin-without-socialmedia-screen__label">Imèl</label><input ref={(node)=>{this.emailInput=node}} type="email" placeholder="Tanpri mete imel ou" className="signin-without-socialmedia-screen__input" required/></div>
                     <div className="signin-without-socialmedia-screen__form-group"><label className="signin-without-socialmedia-screen__label">Paswòd</label><input ref={(node)=>{this.passwordInput=node}} type="password" placeholder="Tanpri mete passwòd ou" className="signin-without-socialmedia-screen__input" required/></div>
                     <div className="signin-without-socialmedia-screen__form-group"><button className="auth-button success-button" type="submit">KONEKTE</button></div>
+                    <ToastContainer/>
                 </form>
             </div>   
         )}
@@ -53,10 +54,7 @@ const SignInWithoutSocialMedia = ({ signIn,writeUserAuthInfoToCache,history })=>
 
 }
 
-
-
 export default compose(
-    graphql(AUTH_WITHOUT_SOCIAL_MEDIA,{name:"signIn"}),
     graphql(WRITE_AUTH_INFO,{name:"writeUserAuthInfoToCache"})
 )(SignInWithoutSocialMedia);
 
