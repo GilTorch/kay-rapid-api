25b34dd039ae19536a23e2dc6fcca2cdb853e0ea
Gilbert Torchon <gilbert.torchon@esih.edu>
2018-12-26 04:49:01 -0600

    deleted client

--- client/src/__tests__/AddHouse.test.js
+++
@@ -1,7 +0,0 @@
-import { shallow } from 'enzyme';
-import AddHouse from '../components/AddHouse'
-
-describe(<AddHouse/>,()=>{
-    const Wrapper=shallow(<AddHouse/>);
-    expect(Wrapper).toContain(<form className="add-form"></form>)
-})
